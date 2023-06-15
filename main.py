900a863223564a2b37431973ab1715a67aec771a
oxoovo <102413837+oxoovo@users.noreply.github.com>
2023-06-15 19:26:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from deta import Deta
 app = Flask(__name__)
 deta = Deta()
 
-identity = deta.Base("user")
+user_base = deta.Base("user")
 
 @app.route("/")
 def index():
     return "This is the PictureNet api."
+    
+@app.route("adduser", methods=["POST"])
+def add_user():
+    request_data = request.get_json()
+    if request_data:
+        if not user_base.get(request_data["id"]):
+            user_base.put({"user": request_data["user"], "url": request_data["url"], "liked": []}, request_data["id"])
+        else:
+            return "error: user existing"
+    else:
+        return "error: no data!"
